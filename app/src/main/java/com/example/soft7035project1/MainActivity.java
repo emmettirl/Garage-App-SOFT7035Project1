997ec4770997e50039ab5a70f5d31eaa56646a64
Emmett <emmett.fitzharris@mycit.ie Profile image emmett.fitzharris@mycit.ie>
2023-10-12 16:42:02 +0100

    Added option selection logic to MainActivity, recognizing which radio option is selected

--- app/src/main/java/com/example/soft7035project1/MainActivity.java
+++ app/src/main/java/com/example/soft7035project1/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.soft7035project1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,8 +21,25 @@ public class MainActivity extends AppCompatActivity {
         buttonStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intentStart = new Intent(MainActivity.this, Recycler_View_Vehicle_Listing.class);
-                startActivity(intentStart);
+
+                RadioButton radioCar = (RadioButton) findViewById(R.id.radioCar);
+                RadioButton radioBike = (RadioButton) findViewById(R.id.radioBike);
+                RadioButton radioOther = (RadioButton) findViewById(R.id.radioOther);
+
+                if(radioCar.isChecked()) {
+                    Intent intentStart = new Intent(MainActivity.this, Recycler_View_Vehicle_Listing.class);
+                    startActivity(intentStart);
+                } else if(radioBike.isChecked()) {
+                    Intent intentStart = new Intent(MainActivity.this, Recycler_View_Vehicle_Listing.class);
+                    startActivity(intentStart);
+                } else if(radioOther.isChecked()) {
+                    Intent intentStart = new Intent(MainActivity.this, Recycler_View_Vehicle_Listing.class);
+                    startActivity(intentStart);
+                } else {
+                    Context context = getApplicationContext();
+                    CharSequence text = "Please Select an option, then click \"Start\".";
+                    Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
