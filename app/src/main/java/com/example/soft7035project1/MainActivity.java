8941ac86e3b62e040826f284c78e415cb61221a4
Emmett <emmett.fitzharris@mycit.ie Profile image emmett.fitzharris@mycit.ie>
2023-10-12 16:12:53 +0100

    Implemented basic navigation between MainActivity and Recycler_View_Vehicle_Listing

--- app/src/main/java/com/example/soft7035project1/MainActivity.java
+++ app/src/main/java/com/example/soft7035project1/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.soft7035project1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonStart = findViewById(R.id.buttonStart);
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentStart = new Intent(MainActivity.this, Recycler_View_Vehicle_Listing.class);
+                startActivity(intentStart);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/soft7035project1/Recycler_View_Vehicle_Listing.java
+++ app/src/main/java/com/example/soft7035project1/Recycler_View_Vehicle_Listing.java
@@ -2,7 +2,12 @@ package com.example.soft7035project1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class Recycler_View_Vehicle_Listing extends AppCompatActivity {
 
@@ -10,5 +15,14 @@ public class Recycler_View_Vehicle_Listing extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_view_vehicle_listing);
+
+        FloatingActionButton fabBack = findViewById(R.id.fabBack);
+        fabBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentBack = new Intent(Recycler_View_Vehicle_Listing.this, MainActivity.class);
+                startActivity(intentBack);
+            }
+        });
     }
 }
