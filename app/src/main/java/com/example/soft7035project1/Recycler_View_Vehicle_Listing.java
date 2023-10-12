fcee74b1d5eaa0d68b5907548eeb78e54776dd56
Emmett <emmett.fitzharris@mycit.ie Profile image emmett.fitzharris@mycit.ie>
2023-10-12 15:04:10 +0100

    Some initial layout configuration

---
+++ app/src/main/java/com/example/soft7035project1/Recycler_View_Vehicle_Listing.java
@@ -0,0 +1,14 @@
+package com.example.soft7035project1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Recycler_View_Vehicle_Listing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_view_vehicle_listing);
+    }
+}
