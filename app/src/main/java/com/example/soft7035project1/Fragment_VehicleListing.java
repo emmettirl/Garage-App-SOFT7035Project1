92506b707dd23c49f4d2f3936c181c3f7a1a1158
Emmett <emmett.fitzharris@mycit.ie Profile image emmett.fitzharris@mycit.ie>
2023-10-13 15:03:23 +0100

    Basic Recycler View Example set up. To be adapted to specific purpose

--- app/src/main/java/com/example/soft7035project1/Fragment_VehicleListing.java
+++ app/src/main/java/com/example/soft7035project1/Fragment_VehicleListing.java
@@ -1,18 +1,45 @@
 package com.example.soft7035project1;
 
+import android.content.Context;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class Fragment_VehicleListing extends Fragment {
+public class Fragment_VehicleListing extends Fragment implements Fragment_VehicleListing_RV_Adapter.ItemClickListener {
+
+    Fragment_VehicleListing_RV_Adapter adapter;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_vehicle_listing, container, false);
+        Context context = getActivity();
+
+
+        ArrayList<String> animalNames=new ArrayList<>();
+        this.adapter = new Fragment_VehicleListing_RV_Adapter(this, animalNames);
+
+
+
+        animalNames.add("Horse");
+        animalNames.add("Sheep");
+        animalNames.add("Goat");
+        animalNames.add("Rabbit");
+
 
         int selectedTab = getArguments().getInt("selectedTab");
 
@@ -26,8 +53,17 @@ public class Fragment_VehicleListing extends Fragment {
             fragmentHeader.setText("Other");
         }
 
+        RecyclerView recyclerView=view.findViewById(R.id.recyclerVehicleListing);
+        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+        adapter.setClickListener(this);
+        recyclerView.setAdapter(adapter);
+
         // Inflate the layout for this fragment
         return view;
+    }
+
+    public void onItemClick(View view, int position){
+        Toast.makeText(getActivity(), "You clicked "+ adapter.getItem(position) +" on row number " + position, Toast.LENGTH_SHORT).show();
     }
 
     public static Fragment_VehicleListing newInstance(int selectedTab) {
@@ -37,4 +73,6 @@ public class Fragment_VehicleListing extends Fragment {
         fragment.setArguments(args);
         return fragment;
     }
+
+
 }
---
+++ app/src/main/java/com/example/soft7035project1/Fragment_VehicleListing_RV_Adapter.java
@@ -0,0 +1,69 @@
+package com.example.soft7035project1;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class Fragment_VehicleListing_RV_Adapter extends RecyclerView.Adapter<Fragment_VehicleListing_RV_Adapter.ViewHolder> {
+
+    private List<String> mData;
+    private LayoutInflater mInflater;
+    private ItemClickListener mClickListener;
+
+    Fragment_VehicleListing_RV_Adapter(Fragment_VehicleListing context, List<String> data) {
+        this.mInflater = LayoutInflater.from(context.getActivity());
+        this.mData = data;
+    }
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.rv_row_vehicle_listing, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        String animal = mData.get(position);
+        holder.RvRowTextView.setText(animal);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView RvRowTextView;
+
+        ViewHolder(View itemview) {
+            super(itemview);
+            RvRowTextView = itemview.findViewById(R.id.RvRowTextView);
+            itemview.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
+
+        }
+
+
+    }
+    String getItem(int id) {
+        return mData.get(id);
+    }
+
+    void setClickListener(ItemClickListener itemClickListener) {
+        this.mClickListener = itemClickListener;
+    }
+
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
+    }
+}
